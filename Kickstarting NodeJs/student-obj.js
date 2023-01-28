7b8176fca7ccd0a8fbc58eff9aea70a95b7a9740
viru <38450997+HRithIkGowDA@users.noreply.github.com>
2023-01-28 12:09:16 +0530

    Create student-obj.js

---
+++ Kickstarting NodeJs/student-obj.js
@@ -0,0 +1,9 @@
+const student = {
+  name: "Hrithik Gowda",
+  age: 23,
+  major: "Computer Science",
+  GPA: 8.5,
+  classes: ["Math", "Physics", "Computer Science"],
+};
+
+console.log(student.name);
