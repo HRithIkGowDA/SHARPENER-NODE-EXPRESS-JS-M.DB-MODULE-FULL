70a16cd386a598e42cee9b0bb704f716ccae7043
viru <38450997+HRithIkGowDA@users.noreply.github.com>
2023-01-29 12:29:54 +0530

    converted to sequelize from SQL

---
+++ ExpressJS/SQL/util/database.js
@@ -0,0 +1,9 @@
+
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('node-complete' , 'root','developer' , {
+    dialect : 'mysql',
+    host : 'localhost'
+})
+
+module.exports = sequelize
